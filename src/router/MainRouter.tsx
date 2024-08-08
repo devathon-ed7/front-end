961fa68b1be419ebefb94d36d132358e509a7b84
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-08-08 15:27:59 -0500

    Feat:design dashboard

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -8,7 +8,7 @@ export const MainRouter = () => {
   return (
     <>
       <Routes>
-        {status === "not-authenticated" ? (
+        {status === "authenticated" ? (
           <>
             <Route path="/auth/*" element={<AuthRoutes />} />
             <Route path="/*" element={<Navigate to="/auth" />} />
