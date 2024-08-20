707b6aa07bb1f28c22b027df74a44fa9fe9e5cd6
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-08-20 10:37:36 -0500

    featura:solution conflict

--- src/interfaces/index.interface.ts
+++ src/interfaces/index.interface.ts
@@ -10,9 +10,22 @@ export type StatusSession = "authenticated" | "not-authenticated" | "checking";
 export interface UserLogin extends Required<Pick<User, "userName">> {
   password: string;
 }
-export interface User {
-  userName?: string;
+export interface User extends UserDetails {
+  id?: string;
+  idUser?: string;
+  username?: string;
+}
+export interface UserDetails {
+  name?: string;
+  email?: string;
   role?: string;
+  role_id?: string;
+  profile_filename?: string | null;
+}
+export interface Roles {
+  id: number;
+  name: string;
+  description: string;
 }
 export interface Product{
   id: number;
