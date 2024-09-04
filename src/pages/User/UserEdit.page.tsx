eab3b8da16e4c15fd4a5b66670416e14a5ccae0b
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-09-04 15:17:18 -0500

    feat: image preview componenet

--- src/pages/User/UserEdit.page.tsx
+++ src/pages/User/UserEdit.page.tsx
@@ -1,8 +1,7 @@
-import { FormUser } from "@/components";
-import { UserFormContainer } from "@/components/Users/UserFormContainer";
-import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useParams } from "react-router-dom";
+import { FormUser, ImagePreview } from "@/components";
+import { UserFormContainer } from "@/components/Users/UserFormContainer";
 
 export const UserEditPage = () => {
   const { id } = useParams();
@@ -32,44 +31,7 @@ export const UserEditPage = () => {
                 isDisabled={false}
               />
               {/* PreviewImageLoaded */}
-              <Box className="preview-image">
-                {file || image ? (
-                  <img
-                    src={file ? URL.createObjectURL(file) : image}
-                    alt="imgProfile"
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                      objectPosition: "center",
-                      border: "2px solid",
-                      borderColor: "info.light",
-                    }}
-                  />
-                ) : (
-                  <Box
-                    sx={{
-                      display: "grid",
-                      placeContent: "center",
-                      width: "100%",
-                      height: "100%",
-                      backgroundColor: "info.main",
-                      border: "2px solid",
-                      borderColor: "info.light",
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        color: "white",
-                        fontWeight: "bold",
-                        textShadow: "0 3px 6px #00000035",
-                      }}
-                    >
-                      Sin imagen
-                    </Typography>
-                  </Box>
-                )}
-              </Box>
+              <ImagePreview file={file} image={image} />
             </Box>
           </Box>
         )}
--- src/pages/User/UserNew.page.tsx
+++ src/pages/User/UserNew.page.tsx
@@ -1,5 +1,5 @@
-import { Box, Typography } from "@mui/material";
-import { FormUser } from "@/components";
+import { Box } from "@mui/material";
+import { FormUser, ImagePreview } from "@/components";
 import { UserFormContainer } from "@/components/Users/UserFormContainer";
 export const UserNewPage = () => {
   return (
@@ -27,44 +27,7 @@ export const UserNewPage = () => {
                 isDisabled={isDisabled}
               />
               {/* PreviewImageLoaded */}
-              <Box className="preview-image">
-                {file ? (
-                  <img
-                    src={URL.createObjectURL(file)}
-                    alt="imgProfile"
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                      objectPosition: "center",
-                      border: "2px solid",
-                      borderColor: "info.light",
-                    }}
-                  />
-                ) : (
-                  <Box
-                    sx={{
-                      display: "grid",
-                      placeContent: "center",
-                      width: "100%",
-                      height: "100%",
-                      backgroundColor: "info.main",
-                      border: "2px solid",
-                      borderColor: "info.light",
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        color: "white",
-                        fontWeight: "bold",
-                        textShadow: "0 3px 6px #00000035",
-                      }}
-                    >
-                      Sin imagen
-                    </Typography>
-                  </Box>
-                )}
-              </Box>
+              <ImagePreview file={file} />
             </Box>
           </Box>
         )}
