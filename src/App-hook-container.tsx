1eedaadde496c79588ce5c4ea165acd68d28e8af
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-03-11 13:44:50 -0500

    add tank stack and tank stack query devtools react

--- src/App-hook-container.tsx
+++ src/App-hook-container.tsx
@@ -4,17 +4,25 @@ import { ThemeProvider } from "./shared/components/theme-provider";
 import { Toaster } from "@/shared/components/UI/sonner";
 import i18n from "@/core/i18n";
 import { I18nextProvider } from "react-i18next";
-
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+const queryClient = new QueryClient()
 function AppHookContainer() {
   return (
-    <ThemeProvider>
-      <AppRouter>
-        <I18nextProvider i18n={i18n} defaultNS={"translation"}>
-          <App />
-          <Toaster />
-        </I18nextProvider>
-      </AppRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <AppRouter>
+          <I18nextProvider i18n={i18n} defaultNS={"translation"}>
+            <App />
+            <Toaster />
+          </I18nextProvider>
+        </AppRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
