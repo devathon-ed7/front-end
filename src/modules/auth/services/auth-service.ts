c3fab7304a2affd0d1535f67d37f5117696feb75
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-03-13 11:05:07 -0500

    refactor: auth service

--- src/modules/auth/services/auth-service.ts
+++ src/modules/auth/services/auth-service.ts
@@ -1,43 +1,30 @@
 import { apiPost } from "@/core/config/axiosConfig";
+import { handleError } from "@/core/utils/handle-error";
 import {
-	User,
-	UserLogin,
-	UserRegister,
+  User,
+  UserLogin,
+  UserRegister,
 } from "@/modules/users/interfaces/user.interface";
-import { t } from "i18next";
 interface AuthResponse {
-	user: User;
-	token: string;
+  user: User;
+  token: string;
 }
 
-export const loginService = async (user: UserLogin): Promise<AuthResponse> => {
-	try {
-		return await apiPost<AuthResponse, UserLogin>("/auth/login", user);
-	} catch (error) {
-		if (error instanceof Error) {
-			const message = error.message;
-
-			if (message.includes("401") || message.includes("404")) {
-				throw new Error(t("exception.emailOrPasswordInvalid"));
-			} else if (message.includes("400")) {
-				throw new Error(t("exception.invalidRequest"));
-			}
-
-			throw new Error(message);
-		}
-		throw new Error(t("exception.unknownError"));
-	}
+export const authService = {
+  login: async (user: UserLogin): Promise<AuthResponse> => {
+    try {
+      return await apiPost<AuthResponse>("/auth/login", user);
+    } catch (error) {
+      throw new Error(handleError(error));
+    }
+  },
+  register: async (user: UserRegister): Promise<AuthResponse> => {
+    try {
+      return await apiPost<AuthResponse>("/auth/register", user);
+    } catch (error) {
+      throw new Error(handleError(error));
+    }
+  },
 };
 
-export const registerService = async (
-	user: UserRegister,
-): Promise<AuthResponse> => {
-	try {
-		return await apiPost<AuthResponse, UserRegister>("/auth/register", user);
-	} catch (error) {
-		if (error instanceof Error) {
-			throw new Error(error.message);
-		}
-		throw new Error(t("exception.unknownError"));
-	}
-};
+export default authService;
